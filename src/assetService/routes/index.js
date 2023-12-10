0834c365ba17a65eb2d48026e975193c5b84a86d
SukruthMS <32141094+SukruthMS@users.noreply.github.com>
2023-12-09 19:54:37 -0500

    Account page related APIs added

--- src/assetService/routes/index.js
+++ src/assetService/routes/index.js
@@ -1,22 +1,25 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const authRouter = require('./authRoutes')
-const roleRouter = require('./roleRoutes')
-const stockRouter = require('./stockRoutes')
-const cryptoRouter = require('./cryptoRoutes')
-const adminRouter = require('./adminRoutes')
-const userRouter = require('./userRoutes')
+const authRouter = require("./authRoutes");
+const roleRouter = require("./roleRoutes");
+const stockRouter = require("./stockRoutes");
+const cryptoRouter = require("./cryptoRoutes");
+const adminRouter = require("./adminRoutes");
+const userRouter = require("./userRoutes");
+const accountRouter = require("./accountRoutes");
 
-router.use("/auth", authRouter)
+router.use("/auth", authRouter);
 
-router.use("/roles", roleRouter)
+router.use("/roles", roleRouter);
 
-router.use("/stocks", stockRouter)
+router.use("/stocks", stockRouter);
 
-router.use("/crypto", cryptoRouter)
+router.use("/crypto", cryptoRouter);
 
-router.use("/admin", adminRouter)
+router.use("/admin", adminRouter);
 
-router.use("/user", userRouter)
+router.use("/user", userRouter);
 
-module.exports = router
+router.use("/account", accountRouter);
+
+module.exports = router;
